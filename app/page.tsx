fe607939bdd7976db6bbbaa6fd58580b3d6398fd
Sivasankar S <crazysiva231@gmail.com>
2024-07-21 11:14:56 +0530

    Format Changes

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,11 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
+            
+            
+            
+            
+            
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -32,6 +37,9 @@ export default function Home() {
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
+
+
+
           alt="Next.js Logo"
           width={180}
           height={37}
